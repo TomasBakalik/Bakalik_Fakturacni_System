1fafee51fd9b3ed7b7e0f00f87f644aab965ae10
tomas <bakalik@spsejecna.cz>
2025-05-11 18:31:35 +0200

    Přidal jsem všechny důležité promněnné pro registraci do třídy RegistraceOkno.

--- src/RegistraceOkno.java
+++ src/RegistraceOkno.java
@@ -1,2 +1,21 @@
+import javax.swing.*;
+
 public class RegistraceOkno {
+
+    private JTextField poleJmeno;
+    private JTextField polePrijmeni;
+    private JTextField poleAdresa;
+    private JTextField poleEmail;
+    private JPasswordField poleHeslo;
+    private JTextField poleTelefon;
+    private JTextField poleId;
+    private JButton tlacitkoRegistrovat;
+
+    public RegistraceOkno(){
+        nastavOkno();
+    }
+
+    public void nastavOkno(){
+
+    }
 }
