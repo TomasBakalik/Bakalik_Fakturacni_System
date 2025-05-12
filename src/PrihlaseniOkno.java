1d5bd2716298c692136791138a92c4403c2af823
tomas <bakalik@spsejecna.cz>
2025-05-12 17:19:58 +0200

    Přidal jsem do třídy PrihlaseniOkno hlavní proměnné.

--- src/PrihlaseniOkno.java
+++ src/PrihlaseniOkno.java
@@ -1,2 +1,9 @@
-public class PrihlaseniOkno {
+import javax.swing.*;
+
+public class PrihlaseniOkno extends JFrame {
+
+    private JTextField poleEmail;
+    private JPasswordField poleHeslo;
+    private JButton tlacitkoPrihlasit;
+    private SpravceFaktur spravceFaktur;
 }
