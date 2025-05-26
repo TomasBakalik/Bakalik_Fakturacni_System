48a4feafcede1989411940eb1ee6e4f43ee5ec36
tomas <bakalik@spsejecna.cz>
2025-05-26 16:59:02 +0200

    Přidal jsem komentáře ke třídě GeneratorTxt a k hlavním metodám.

--- src/HlavniOkno.java
+++ src/HlavniOkno.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Tato třída reprezentuje hlavní okno programu.
+ * Uživatel si zde může vybrat, jestli chce vystavit fakturu, zaregistrovat se nebo přihlásit.
+ */
 public class HlavniOkno extends JFrame {
 
     private JButton tlacitkoVystavit;
@@ -14,6 +18,10 @@ public class HlavniOkno extends JFrame {
         setVisible(true);
     }
 
+    /**
+     * Nastaví vlastnosti hlavního okna, pozadí, tlačítka a jejich akce.
+     * Kliknutí na tlačítka vede do dalších částí aplikace podle výběru možnosti (otevírají další okna).
+     */
     public void nastavOkno(){
         setTitle("Fakturační Systém");
         setSize(1200,800);
@@ -51,6 +59,13 @@ public class HlavniOkno extends JFrame {
 
     }
 
+    /**
+     * Vytvoří tlačítko se zadaným textem a barvou pozadí.
+     * Tlačítko má jednotný styl a velikost.
+     * @param text Text, který se zobrazí na tlačítku.
+     * @param barvaPozadi Barva, kterou bude mít tlačítko.
+     * @return Vrací vytvořené tlačítko.
+     */
     public JButton vytvorTlacitko(String text, Color barvaPozadi){
         JButton tlacitko = new JButton(text);
         tlacitko.setPreferredSize(new Dimension(250, 50));
