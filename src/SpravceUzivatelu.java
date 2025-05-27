57ed3966ce3f536280c7703e3db718a2e57bb95b
tomas <bakalik@spsejecna.cz>
2025-05-27 15:34:33 +0200

    Přidal jsem komentáře ke třídě SpravceUzivatelu a hlavním metodám v této třídě.

--- src/SpravceUzivatelu.java
+++ src/SpravceUzivatelu.java
@@ -1,10 +1,19 @@
 import java.io.*;
 import java.util.ArrayList;
 
+/**
+ * Tato třída slouží k práci s uživateli.
+ * Umožňuje ukládat nové uživatele do souboru, načítat je zpět a ověřovat jejich přihlášení podle e-mailu a hesla.
+ */
 public class SpravceUzivatelu {
 
     private String nazevSouboru = "uzivatele.txt";
 
+    /**
+     * Ukládá uživatele do txt souboru.
+     * Údaje jsou oddělené znakem | a zapisují se na nový řádek.
+     * @param uzivatel Uživatel, který se má uložit.
+     */
     public void ulozUzivatele(Uzivatel uzivatel) {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(nazevSouboru, true));
@@ -24,6 +33,12 @@ public class SpravceUzivatelu {
         }
     }
 
+    /**
+     * Načte všechny uživatele ze souboru a vrátí je jako array list.
+     * Každý uživatel musí mít správně vyplněných 7 údajů.
+     * Pokud soubor neexistuje vrací se prázdný seznam.
+     * @return Vrací seznam uživatelů.
+     */
     public ArrayList<Uzivatel> nacistVsechnyUzivatele() {
         ArrayList<Uzivatel> uzivatele = new ArrayList<>();
         File soubor = new File(nazevSouboru);
@@ -60,6 +75,12 @@ public class SpravceUzivatelu {
         return uzivatele;
     }
 
+    /**
+     * Hledá uživatele podle zadaného emailu a hesla.
+     * @param email E-mail zadaný při přihlášení.
+     * @param heslo Heslo zadané při přihlášení.
+     * @return Když najde uživatele, vrátí ho, když ne, vrátí null.
+     */
     public Uzivatel prihlas(String email, String heslo) {
         for (Uzivatel u : nacistVsechnyUzivatele()) {
             if (u.getEmail().equalsIgnoreCase(email) && u.jeSpravneHeslo(heslo)) {
