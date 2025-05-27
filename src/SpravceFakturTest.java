df701f33b994d5c3e1fe66e974551e65da756ebf
tomas <bakalik@spsejecna.cz>
2025-05-27 15:25:04 +0200

    Přidal jsem komentáře ke třídě SpravceFakturTest a hlavním metodám v této třídě.

--- src/SpravceFakturTest.java
+++ src/SpravceFakturTest.java
@@ -3,11 +3,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.Date;
 
+/**
+ * Tato třída ověřuje, jestli se faktura správně přidá do seznamu.
+ */
 public class SpravceFakturTest {
 
     private SpravceFaktur spravce;
     private Faktura faktura;
 
+    /**
+     * Před každým testem vytvoří nového správce faktur a jednu testovací fakturu, která obsahuje jednoho klienta a jednu položku.
+     */
     @BeforeEach
     public void setUp() {
         spravce = new SpravceFaktur();
@@ -18,6 +24,9 @@ public class SpravceFakturTest {
         faktura.pridatPolozku(p);
     }
 
+    /**
+     * Testuje, jestli se po přidání faktury správně zvýší počet faktur na 1 a ověřuje, že se do seznamu opravdu uložila faktura se zadaným číslem.
+     */
     @Test
     public void testPridaniFaktury() {
         System.out.println("Test pro přidání faktury do seznamu");
